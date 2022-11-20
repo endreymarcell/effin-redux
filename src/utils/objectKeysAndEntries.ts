ba553f72271a329cd466d2ee7ae8fbf0179fa5e3
Marcell Endrey <endrey.marcell@gmail.com>
2022-11-20 22:20:51 +0100

    add type-safe object helpers

---
+++ src/utils/objectKeysAndEntries.ts
@@ -0,0 +1,29 @@
+/**
+ * Type-safe wrappers around Object.keys() and Object.entries()
+ *
+ * In TypeScript, the return type of `Object.keys(object)` is always `string[]`, and not `keyof typeof object`.
+ * Consider this code snippet:
+ * ```
+ * for (const key of Object.keys(object)) {
+ *   console.log(object[key]);
+ * }
+ * ```
+ * You would expect this to work without problems: you acquired `key` via `Object.keys()`, so `object[key]` should be fine.
+ * Unfortunately, it isn't, because TS infers the type of `Object.keys()` as `string[]`.
+ * Therefore `key` is `string`, so it's not safe to try to grab `object[key]`.
+ * Use this helper instead of `Object.key()` to overcome this and get the proper return type.
+ *
+ * See also: https://github.com/Microsoft/TypeScript/issues/12870
+ */
+
+export function typedObjectKeys<Object extends { [key: string | number | symbol]: any } | null>(
+  object: Object,
+): Array<keyof Object> {
+  return !!object ? (Object.keys(object) as Array<keyof Object>) : [];
+}
+
+export function typedObjectEntries<Object extends { [key: string | number | symbol]: any } | null>(
+  object: Object,
+): Array<[keyof Object, any]> {
+  return !!object ? (Object.entries(object) as Array<[keyof Object, any]>) : [];
+}
