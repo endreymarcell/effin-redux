af13c4b511978ade36a22e62b406cab3c1f0500d
Marcell Endrey <endrey.marcell@gmail.com>
2022-12-14 18:09:42 +0100

    adjust what we want to expose

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
+export { configureStore } from "./state/appStore";
+export { combineSlices, getInitialState } from "./state/combineSlices";
 export { createReducers, createExtraReducers } from "./state/reducerHelpers";
 export { withEffects } from "./effects/withEffects";
 export type { StateWithEffects } from "./effects/withEffects";
