1f87bc2760ea4c5cea8405c903eb9bc60c61c17e
Marcell Endrey <endrey.marcell@gmail.com>
2022-12-25 21:32:52 +0100

    fix: remove comment that is not irrelevant

--- src/demo-app/slices/counter.ts
+++ src/demo-app/slices/counter.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { createExtraReducers, createReducers } from "../../lib";
 import { createEffectInputs, createEffects, forSlice } from "../../lib/effects/createEffects";
-import { addEffect } from "../app"; // For some reason, vitest doesn't understand $app
+import { addEffect } from "../app";
 
 export type CounterState = {
   count: number;
