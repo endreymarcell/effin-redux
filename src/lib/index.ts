83124579bdaaf86eaedf8d560a662ade4f1c44b4
Marcell Endrey <endrey.marcell@gmail.com>
2022-12-25 13:19:18 +0100

    expose getHelpers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,4 @@ export { createReducers, createExtraReducers } from "./state/reducerHelpers";
 export { withEffects } from "./effects/withEffects";
 export type { StateWithEffects } from "./effects/withEffects";
 export { setStoreForScheduledEffects } from "./effects/scheduling";
+export { getHelpers } from "./helpers";
