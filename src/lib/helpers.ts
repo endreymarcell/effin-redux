10b6405b4185cfa93169a00be1bb077b2eff5ead
Marcell Endrey <endrey.marcell@gmail.com>
2022-12-30 23:59:49 +0100

    fix: cannot assume presence of effects field on state

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,7 +1,6 @@
 export function getHelpers<AppState>() {
   return {
-    addEffect: <SliceState extends { $$effects?: any[] }, Effect>(state: SliceState, effect: Effect) =>
-      void (state.$$effects = [...(state.$$effects ?? []), effect]),
+    addEffect: <Effect>(state: any, effect: Effect) => void (state.$$effects = [...(state.$$effects ?? []), effect]),
     readAppState: (state: any): AppState => state.$$appState,
   };
 }
